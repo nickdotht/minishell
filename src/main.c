b085209836be3a6333fcc263524479896cc92e8a
Nick Rameau <r4meau@gmail.com>
2017-04-24 06:00:23 -0700

    Update .gitignore, add author file, add fork example

--- src/main.c
+++ src/main.c
@@ -1,18 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrameau <jrameau@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/24 04:17:47 by jrameau           #+#    #+#             */
+/*   Updated: 2017/04/24 05:46:36 by jrameau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int	main(int ac, char **av) {
-	pid_t	father;
-
 	(void)ac;
-	father = fork();
-	if (father > 0) {
-		wait(&father);
-		printf("I'm your father!\n");
+	(void)av;
+	printf("I am %d\n", (int)getpid());
+	pid_t pid = fork();
+	if (pid == 0) {
+		printf("Do something big in the child\n");
+		sleep(5);
+		printf("Child ending\n");
+		exit(42);
 	}
-	if (father == 0) {
-		sleep(60);
-		execve("/bin/ls", av, NULL);
+	else if (pid < 0) {
+		printf("Something wrong happened\n");
 	}
+	printf("Do some other big stuff while child is working\n");
+	wait(&pid);
+	printf("Child returned %d\n", WEXITSTATUS(pid));	
+	printf("Do something else after child is done\n");
+	return (0);
 }
