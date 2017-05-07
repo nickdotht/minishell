e242504b0122bd62ef18f49ded27a228065768a6
Nick Rameau <r4meau@gmail.com>
2017-05-07 01:13:19 -0700

    WIP: Add echo builtin

--- src/builtins.c
+++ src/builtins.c
@@ -2,67 +2,38 @@
 
 void echo_builtin(char **command)
 {
-	int i;
-	int len;
+	int		i;
+	int		n_flag;
 
 	i = -1;
-	while (command[++i]) {
-		len = ft_strlen(command[i]);
-		if (command[i][0] == '"' && command[i][len - 1] == '"')
-			ft_putnstr(command[i] + 1, len - 1);
-		else if (command[i][0] == '"')
-			ft_putstr(command[i] + 1);
-		else if (command[i][len - 1] == '"')
-			ft_putnstr(command[i], len - 1);
-		else
-			ft_putstr(command[i]);
-		if (command[i + 1])
-			ft_putchar(' ');
-	}
-	ft_putchar('\n');
-}
-
-int count_args(char *str)
-{
-	int i;
-	int count;
-
-	i = -1;
-	count  = 0;
-	while (str[++i]) {
-		while (str[i] == ' ')
-			i++;
-		count++;
-		if (str[i++] == '"') {
-			while (str[i] != '"')
-				i++;
-		} else {
-			while (str[i] != ' ')
-				i++;
+	n_flag = 0;
+	while (command[++i])
+	{
+		if (i == 0 && command[0][0] == '-')
+		{
+			if (command[0][1] == 'n')
+				n_flag = 1;
 		}
-	}
-	return (count);
+		else
+		{
+			ft_putstr(command[i]);
+			if (command[i + 1])
+				ft_putchar(' ');
+		}
+		if (!command[i + 1])
+		{
+			if (n_flag)
+				ft_putchar('%');
+			ft_putchar('\n');
+		}
+  	}
 }
-
-// char **split_args(char *input)
-// {
-// 	int i = -1;
-// 	char **res;
-
-// 	res = (char **)ft_memalloc(sizeof(char) * count_args(input));
-// 	while (input[++i])
-// 	{
-// 		while 
-// 	}
-// }
 
 int check_builtins(char *input)
 {
 	char **command;
 
-	// command = split_args(input);
-	printf("Num args: %d\n", count_args(input));
-	exit(0);
+	command = ft_strsplit(input, ' ');
 	if (ft_strequ(command[0], "exit")) {
 		// Clean stuff here first (memory leaks)
 		exit(0);
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrameau <jrameau@student.42.us.org>        +#+  +:+       +#+        */
+/*   By: jrameau <jrameau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/24 04:17:47 by jrameau           #+#    #+#             */
-/*   Updated: 2017/04/26 19:11:23 by jrameau          ###   ########.fr       */
+/*   Updated: 2017/05/06 19:52:30 by jrameau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	main(int ac, char **av, char **envv) {
 	(void)ac;
 	(void)av;
 	char *input;
-	char **command;
 	int ret;
 
 	while (1)
